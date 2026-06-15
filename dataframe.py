6c353183928693c38db1c3ed0fedd573219682d2
Arittra Sarkar <blackriders.1409@gmail.com>
2026-06-15 18:28:20 +0530

    Updated ages 2nd

--- dataframe.py
+++ dataframe.py
@@ -1,7 +1,7 @@
 import pandas as pd
 data = {
     'Name': ['Alice', 'Bob', 'Charlie', 'David'],
-    'Age': [12, 25, 45, 60],
+    'Age': [12, 25, 105, 70],
     'City': ['New York', 'Los Angeles', 'Chicago', 'Houston']
 }
 df = pd.DataFrame(data)
